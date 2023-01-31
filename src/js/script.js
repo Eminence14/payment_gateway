b149abf161e82f6beeaff6e60ae87c6210609b1e
Victor <victoribidunmoye@gmail.com>
2023-01-31 03:19:40 +0100

    add card interactivity

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,10 @@
+const reset = document.querySelector('button[type="reset"]')
+reset.addEventListener('click', () => {
+    set_details('Jane Appleseed', '0000 0000 0000 0000', '00', '00', '000')
+    card_display();
+})
+card_display();
+
 
 function spacer() {
     let input = document.getElementById('card_number').value
@@ -15,6 +22,8 @@ let isBlank = false;
 submit.addEventListener('click', e => {
     e.preventDefault()
 
+
+
     // Verify if there is no string in the card number
 
     const cardNum = document.getElementById('card_number').value;
@@ -26,11 +35,13 @@ submit.addEventListener('click', e => {
     if (hasString) {
         cardNumSpan.style.display = 'block';
         cardNumInput.classList.add('unapproved')
+        return;
     } else {
         cardNumSpan.style.display = 'none';
         cardNumInput.classList.remove('unapproved')
     }
     hasString = false;
+
 
 
     // verify if dates are not blank
@@ -49,20 +60,34 @@ submit.addEventListener('click', e => {
     }
     if (month.value === '' || year.value === '') {
         blanker.style.display = 'block'
+        return
     } else {
         blanker.style.display = 'none'
     }
+
 
 
     // verify if cvc is not blank
     const [cvcSpan, cvcInp] = getSpan('cvc')
     if (cvcInp.value === '') {
         cvcSpan.style.display = 'block';
-        cvcInp.classList.add('unapproved')
+        cvcInp.classList.add('unapproved');
+        return;
     } else {
         cvcSpan.style.display = 'none';
         cvcInp.classList.remove('unapproved')
     }
+
+
+
+    // store values in the local storage
+    const name = document.getElementById('name').value;
+    const form_month = month.value;
+    const form_year = year.value;
+    const form_cvc = cvcInp.value;
+
+    set_details(name, cardNum, form_month, form_year, form_cvc)
+    card_display();
 })
 
 
@@ -73,4 +98,36 @@ function getSpan(input) {
     const span = Array.from(div)[2]
     const inp = Array.from(div)[1]
     return [span, inp]
+}
+
+
+
+// a function to set values on the card from the local storage
+
+function set_details(name, num, mon, yr, cvc) {
+    localStorage.setItem('name', name);
+    localStorage.setItem('card_number', num)
+    localStorage.setItem('month', mon)
+    localStorage.setItem('year', yr)
+    localStorage.setItem('cvc', cvc)
+}
+
+
+
+// a function to display the localStorage values on the card
+function card_display() {
+    // get current card details
+    const cvc = document.querySelector('.card_cvv')
+    const card_number = document.querySelector('.card_number');
+    const card_name = document.querySelector('.card_name');
+    const card_month = document.querySelector('#card_exp_month')
+    const card_year = document.querySelector('#card_exp_year')
+
+
+    // set their text content from the localStorage
+    cvc.textContent = localStorage.getItem('cvc')
+    card_number.textContent = localStorage.getItem('card_number')
+    card_name.textContent = localStorage.getItem('name')
+    card_month.textContent = localStorage.getItem('month')
+    card_year.textContent = localStorage.getItem('year')
 }
